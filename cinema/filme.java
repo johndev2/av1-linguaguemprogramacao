fc432a3ba0662ec1bdec3bdb4adcc73de065542e
John Carvalho <87444965+johndev2@users.noreply.github.com>
2022-09-24 10:53:33 -0300

    novo

--- cinema/filme.java
+++ cinema/filme.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package cinema;
 
@@ -8,17 +8,18 @@ package cinema;
  *
  * @author johnv
  */
-public class Cinema {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class filme {
+    public String titulo_orig;
+    public String titulo_trad;
+    public String genero;
+    public String atores;
+    public String diretor;
+    public String sinopse;
+    public String nacionalidade;
+    public float tam_filme;
+    public int Id;
     
 }
-
 
 /*
 classe cinema ( localiza no brasil )
